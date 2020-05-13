134266a7d7d930375edf6662fd3dc2bc4f8bc1f1
Asiri Iroshan <asiriiroshan@localhost.localdomain>
2020-05-13 05:46:25 +0530

    updated. Multiple simulators added

--- API/app/controllers/user.controller.js
+++ API/app/controllers/user.controller.js
@@ -1,4 +1,6 @@
 const User = require("../models/user.model.js");
+const bcrypt = require("bcrypt");
+const json = require("json");
 exports.create = (req,res) =>{
     
     if(!req.body){
@@ -6,39 +8,46 @@ exports.create = (req,res) =>{
             message : "Content cannot be empty"
         });
     }else{
-        const newRecord = new User({
-            NIC:req.body.NIC,
-            FirstName:req.body.FirstName,
-            MiddleName:req.body.MiddleName||"Not Available",
-            Surname:req.body.Surname,
+        bcrypt.hash(req.body.Password,10,(err,hashedPassword)=>{
+            if(err){
+                console.log(err);
+                res.status(500).send({
+                    message : err.message || "Error Hashing Password"
+                });
+            }else{
+                const newRecord = new User({
+                    NIC:req.body.NIC,
+                    FirstName:req.body.FirstName,
+                    MiddleName:req.body.MiddleName||"Not Available",
+                    Surname:req.body.Surname,
+                
+                    DOB:req.body.DOB,
+                    
+                    MobilePhone:req.body.MobilePhone||"Not Available",
+                    HomeTelephone:req.body.HomeTelephone||"Not Available",
+                    
+                    Email:req.body.Email||"Not Available",
+                    
+                    AddressLine1:req.body.AddressLine1,
+                    AddressLine2:req.body.AddressLine2||"Not Available",
         
-            DOB:req.body.DOB,
+                    Hospital:req.body.Hospital,
+                    Ward:req.body.Ward||"Not Applicable",  
+                    PatientType:req.body.PatientType,
+                    DeviceID:req.body.DeviceID,
+                    AccountType:req.body.AccountType,
+                    Password:hashedPassword, 
             
-            MobilePhone:req.body.MobilePhone||"Not Available",
-            HomeTelephone:req.body.HomeTelephone||"Not Available",
-            
-            Email:req.body.Email||"Not Available",
-            
-            AddressLine1:req.body.AddressLine1,
-            AddressLine2:req.body.AddressLine2||"Not Available",
-
-            Hospital:req.body.Hospital,
-            Ward:req.body.Ward,  
-            PatientType:req.body.PatientType,
-            DeviceID:req.body.DeviceID,
-            AccountType:req.body.AccountType,
-            Password:req.body.Password, 
-    
-            AreaOfExpertise:req.body.AreaOfExpertise||"Not Applicable"
-
-            
-        });
-        newRecord.save().then(data => {
-            res.send(data);
-        }).catch(err=>{
-            res.status(500).send({
-                message : err.message || "Error"
-            });
+                    AreaOfExpertise:req.body.AreaOfExpertise||"Not Applicable"    
+                });
+                newRecord.save().then(data => {
+                    res.send(data);
+                }).catch(err=>{
+                    res.status(500).send({
+                        message : err.message || "Error"
+                    });
+                });
+            }
         });
     }
 };
@@ -54,25 +63,17 @@ exports.findAll = (req,res) =>{
     });
 }
 exports.findOne = (req,res) =>{
-    User.findById(req.params.nic).then(data=>{
-        if(!data){
-            return res.status(404).send({
-                message : "Record not found"
+    User.find({NIC:req.params.nic},(err,data)=>{
+        if(err){
+            res.status(500).send({
+                message : err.message ||"Error"
             });
+        }else if(data){
+            res.send(data);
         }else{
-            return res.send(data);
+            res.send({message:"Record not found"});
         }
-    }).catch(err=>{
-        if(err.kind === 'ObjectId'){
-            return res.status(404).send({
-                message : "Record not found"
-            });
-        }else{
-            return res.status(500).send({
-                message : err.message || "Error"
-            });
-        }
-    });
+    })
 };
 exports.update = (req,res) =>{
     if(!req.body){
@@ -80,46 +81,58 @@ exports.update = (req,res) =>{
             message : "Content cannot be empty"
         });
     }else{
-        var newRecord = 
-            {
-                FirstName:req.body.FirstName,
-                MiddleName:req.body.MiddleName||"Not Available",
-                Surname:req.body.Surname,
-            
-                DOB:req.body.DOB,
-                
-                MobilePhone:req.body.MobilePhone||"Not Available",
-                HomeTelephone:req.body.HomeTelephone||"Not Available",
-                
-                Email:req.body.Email||"Not Available",
-                
-                AddressLine1:req.body.AddressLine1,
-                AddressLine2:req.body.AddressLine2||"Not Available",
-
-                Hospital:req.body.Hospital,
-                Ward:req.body.Ward,   
-    
-                AreaOfExpertise:req.body.AreaOfExpertise
-                
-            };
-        
-            User.findByIdAndUpdate(req.params.nic,newRecord,{new:true},(err,data)=>{
+        bcrypt.hash(req.body.Password,10,(err,hashedPassword)=>{
             if(err){
                 console.log(err);
                 res.status(500).send({
-                    message : "An error occured. Please see the console logs"
+                    message : err.message || "Error Hashing Password"
                 });
-            }else if(data){
-                res.send(data);
             }else{
-                res.status(404).send({
-                    message : "Record does not exist"
-                });
+                const newRecord = 
+                {
+                    FirstName:req.body.FirstName,
+                    MiddleName:req.body.MiddleName||"Not Available",
+                    Surname:req.body.Surname,
+                
+                    DOB:req.body.DOB,
+                    
+                    MobilePhone:req.body.MobilePhone||"Not Available",
+                    HomeTelephone:req.body.HomeTelephone||"Not Available",
+                    
+                    Email:req.body.Email||"Not Available",
+                    
+                    AddressLine1:req.body.AddressLine1,
+                    AddressLine2:req.body.AddressLine2||"Not Available",
+    
+                    Hospital:req.body.Hospital,
+                    Ward:req.body.Ward,   
+                    PatientType:req.body.PatientType,
+                    Password:hashedPassword,
+                    DeviceID:req.body.DeviceID,
+                    AccountType:req.body.AccountType,
+                    AreaOfExpertise:req.body.AreaOfExpertise
+                    
+                };
+               User.updateOne({NIC:req.params.nic},newRecord,{new:true},(err,data)=>{
+                    if(err){
+                        res.status(500).send({
+                            message : err.message || "Error"
+                        });
+                        console.log(err);
+                    }else if(data.n === 1){
+                        console.log("Updated");
+                        res.send({message:"Updated"});
+                    }else{
+                        console.log("Record not found");
+                        res.send({message:"Record not found"});
+                    }
+               });
             }
         });
+       
     }
 }
-exports.delete = (req,res) =>{
+/*exports.delete = (req,res) =>{
     User.findByIdAndDelete(req.params.nic).then(data=>{
         if(!data){
             return res.status(404).send({
@@ -141,7 +154,24 @@ exports.delete = (req,res) =>{
             });
         }
     });
-};
+};*/
+exports.delete=(req,res)=>{
+    User.deleteOne({NIC:req.params.nic},(err,data)=>{
+        if(err){
+            res.status(500).send({
+                message : err.message || "Error"
+            });
+        }else if(data.n === 1){
+            res.send({
+                message : `Record under NIC ${req.params.nic} was successfully deleted`
+            });
+        }else{
+            res.send({
+                message : `No record was found for NIC ${req.params.nic}`
+            })
+        }
+    });
+}
 exports.deleteAll = (req,res) =>{
     User.deleteMany({},(err,data)=>{
         if(err){return res.status(500).send({
